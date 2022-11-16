ac88baabd105ff08c9958bbc6d8b9a48f955a1a3
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-11-16 15:37:16 -0800

    app with word API call for definitions
    Co-authored-by: Vera Weikel <vera.weikel@gmail.com>
    Co-authored-by: Yodel Guanzon <yodelguanzon@gmail.com>
    Co-authored-by: Richard Cha <charichard09@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Word from './word.js';
+import WordService from './word.js';
+
+function getWord(word) {
+  let promise = WordService.getWord(word);
+
+  promise.then(function(shortDefinitions) {
+    printElements(shortDefinitions);
+  }, function(error) {
+    printError(error);
+  });
+}
+
+function printElements(shortDefinitions) {
+  for (const definition of shortDefinitions) {
+  document.querySelector("#showResponse").innerText += definition + "\n";
+  }
+}
+
+function printError(error) {
+  document.querySelector("#showResponse").innerText = error;
+}
 
 function handleFormSubmission(event) {
   event.preventDefault();
 
+  const word = document.getElementById("word").value;
+  document.getElementById("word").value = null;
+  getWord(word);
 }
 
 window.addEventListener("load", function() {
--- src/word.js
+++ src/word.js
@@ -2,9 +2,22 @@ export default class WordService {
   static getWord(word) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${APP_KEY}`
+      const url = `https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.APP_KEY}`;
+
+      request.addEventListener("loadend", function() {
+        let response; 
+        if (this.status === 200) {
+          response = JSON.parse(this.responseText);
+          resolve(response[0].shortdef)
+        } else {
+          response = this.responseText;
+          reject(response);
+        }
+      });
+
+      request.open("GET", url, true);
+      request.send();
     });
   }
-
 }
 
