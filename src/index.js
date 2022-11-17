5c131c4f3ff547d54cc5e485fad1e07c67f6ccc4
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-17 13:26:48 -0800

    It will update fetch promise and implement try.catches on index.js for err handling.
    Co-authored-by: Yodel Guanzon <yodelguanzon@gmail.com>
    Co-authored-by: Jennifer Holcomb  <jennifer.holcomb@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,45 +2,56 @@
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/styles.css';
 import WordService from './service/word.js';
-import GiphyService from './service/giphy-service.js';
+// import GiphyService from './service/giphy-service.js';
 
-async function getWord(word) {
-  const response = WordService.getWord(word);
-  //let wordDef = promise[0].shortdef;
-  let shortDefinitions = response.shortDefinition
-  if(shortDefinitions) {
-    printElements(shortDefinitions);
-  }else {
-    printError(response);
-  }
-}
-
-function printElements(shortDefinitions) {
-  const shortDef = shortDefinitions[0].shortdef;
-  for (const definition of shortDef) {
-  document.querySelector("#showResponse").innerText += definition + "\n\n";
-  }
-}
-
-async function getAudio(word) {
-  let promise = WordService.getWord(word);
-  promise.then(function(response) {
-    addAudio(response[0].hwi.prs[0].sound.audio);
-  }, function(error) {
+async function getWord(word) { 
+  //fetch > promise .then > response
+  const promise = WordService.getWord(word);
+  promise.then(function (response) {
+    printElements(response);
+  }, function (error) {
     printError(error);
   });
 }
 
-function addAudio(sound) {
-  let div = document.getElementById("auDiv");
-  let audio = document.createElement("audio");
-  let src = document.createElement("source");
-  const srcTxt = `https://media.merriam-webster.com/audio/prons/en/us/mp3/${sound[0]}/${sound}.mp3`
-  src.setAttribute("src", srcTxt);
-  src.setAttribute("type", "audio/mp3");
-  audio.setAttribute("controls","");
-  audio.append(src);
-  div.replaceChildren(audio);
+function printElements(shortDefinitions) {
+  try {
+    const shortDef = shortDefinitions[0].shortdef;
+    for (const definition of shortDef) {
+      document.querySelector("#showResponse").innerText += definition + "\n\n";
+    }
+  }
+  catch (error) {
+    document.querySelector("p#showResponse").innerText += "This is not available."
+  }
+}
+
+
+async function getAudio(word) {
+  let promise = WordService.getWord(word);
+  promise.then(function (response) {
+    addAudio(response);// response[0].hwi.prs[0].sound.audio);
+  }, function (error) {
+    printError(error);
+  });
+}
+
+function addAudio(response) { // sound
+  try {
+    let audioWord = response[0].hwi.prs[0].sound.audio;
+    let div = document.getElementById("auDiv");
+    let audio = document.createElement("audio");
+    let src = document.createElement("source");
+    const srcTxt = `https://media.merriam-webster.com/audio/prons/en/us/mp3/${audioWord[0]}/${audioWord}.mp3`;
+    src.setAttribute("src", srcTxt);
+    src.setAttribute("type", "audio/mp3");
+    audio.setAttribute("controls", "");
+    audio.append(src);
+    div.replaceChildren(audio);
+  }
+  catch (error) {
+    document.querySelector("p#showResponse").innerText = " No audio response."
+  }
 }
 
 function printError(error) {
@@ -57,6 +68,6 @@ function handleFormSubmission(event) {
   getAudio(word);
 }
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   document.querySelector("form").addEventListener("submit", handleFormSubmission);
 });
--- src/service/word.js
+++ src/service/word.js
@@ -1,19 +1,19 @@
 export default class WordService {
-  static async getWord(word) {
-    try {
-      const response = await fetch(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.API_KEY}`);
-      const jsonifiedResponse = await response.json();
-      if (!response.ok) {
-        const errorMessage = `${jsonifiedResponse}`; //${response.status}${response.statusText} 
-        //  let response = errorMessage;
-        // const errorMessage = `${response[0]}`           
-        throw new Error(errorMessage);
-      }
-      return jsonifiedResponse;
-    }
-    catch (error) {
-      return error;
-    }
+  static getWord(word) {
+    return fetch(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.DICT_API_KEY}`)
+      .then(function(response){
+        if (!response.ok) {
+          const errorMessage = `${response.statusText}`; //${response.status}${response.statusText} 
+          //  let response = errorMessage;
+          // const errorMessage = `${response[0]}`           
+          throw new Error(errorMessage);
+        } else {
+          return response.json();
+        }
+      })
+      .catch (function(error) {
+        return error;
+      });
   }
 }
 
