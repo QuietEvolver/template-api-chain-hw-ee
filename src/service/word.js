3761fda6fe8497a26397c1b41f5fac826b16b334
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-17 07:20:06 -0800

    It will update api promise to a fetch.

--- src/service/word.js
+++ src/service/word.js
@@ -1,23 +1,18 @@
 export default class WordService {
   static getWord(word) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.API_KEY}`;
-
-      request.addEventListener("loadend", function() {
-        let response; 
-        if (this.status === 200) {
-          response = JSON.parse(this.responseText);
-          resolve(response)
+    return fetch(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.API_KEY}`)
+      .then(function(response) {
+        if (!response.ok) {
+        const errorMessage = `${response.status}${response.statusText}`;
+        //  let response =errorMessage;
+        throw new Error(errorMessage);
         } else {
-          response = this.responseText;
-          reject(response);
+        return response.json();
         }
+      })
+      .catch(function(error){
+        return error;
       });
-
-      request.open("GET", url, true);
-      request.send();
-    });
   }
 }
 
