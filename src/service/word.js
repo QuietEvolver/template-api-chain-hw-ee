da6a7b40c54ca152b647f933a20811dddb00a182
Vera Weikel <veraweikel@Veras-MacBook-Pro-3.local>
2022-11-17 10:05:09 -0800

    It will update the error handling in word.js and in fx's to print to DOM in try.catch on index.js.
    Co-authored-by: Yodel Guanzon <yodelguanzon@gmail.com>
    Co-authored-by: Jennifer Holcomb  <jennifer.holcomb@gmail.com>

--- src/service/word.js
+++ src/service/word.js
@@ -1,19 +1,14 @@
 export default class WordService {
   static getWord(word) {
     return fetch(`https://dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${process.env.API_KEY}`)
-      .then(function (response) {
+      .then(function(response) {
         if (!response.ok) {
-          return response.json()
-        .then(function (apiErrorMessage) {
-            const errorMessage = `${response.status}${response.statusText} ${apiErrorMessage}`;
-            //  let response = errorMessage;
-           // const errorMessage = `${response[0]}`           
-            throw new Error(errorMessage);
-          });
+          const errorMessage = `${response}`;//`${response.status} ${response.statusText}`;
+          throw new Error(errorMessage);
         } else {
-            return response.json();
-          }
-        })
+          return response.json();
+        }
+      }) 
       .catch(function (error) {
         return error;
       });
